13c4c205df01801c0c881dae2317a84fdc9d774d
Paul Friederichsen <floogulinc@gmail.com>
2015-04-20 09:22:21 -0500

    Added For Loop assignment

---
+++ For Loop/src/forloop/ForLoop.java
@@ -0,0 +1,13 @@
+//Paul Friederichsen
+package forloop;
+
+public class ForLoop {
+	
+	public static void main(String[] args) {
+		//for(initial value; condition; increment/decrement)
+		for(int i = 10; i > 0; i--) {
+			System.out.println("i = " + i);
+		}
+	}
+
+}
