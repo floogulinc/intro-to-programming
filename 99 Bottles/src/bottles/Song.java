721d5062db146a18bfd97c9e20eaa982763544f0
Paul Friederichsen <floogulinc@gmail.com>
2015-04-21 09:51:29 -0500

    Added SongThatDoesntEnd assignment and added comment to 99 bottles assignment

--- 99 Bottles/src/bottles/Song.java
+++ 99 Bottles/src/bottles/Song.java
@@ -1,7 +1,8 @@
 package bottles;
 
 public class Song {
-
+	
+	//Totally done wrong, assignment was for a while loop, but this is better.
 	public static void main(String[] args) {
 		for(int i = 99; i > 0; i--) System.out.println(i + (i==1 ? " bottle" : " bottles") + " of pop on the wall, " + i + (i==1 ? " bottle" : " bottles") + " of pop\nYou take one down and pass it around, " + (i==1 ? "no more bottles of pop on the wall" : (i - 1) + (i==1 ? " bottle" : " bottles") + " of pop on the wall"));
 	}
