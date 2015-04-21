348f3d44811c6a3b874de45dffd83ec394f292fc
Paul Friederichsen <floogulinc@gmail.com>
2015-04-21 09:56:18 -0500

    Added change to SongThatDoesntEnd assignment

--- For Loop/src/song/SongThatDoesntEnd.java
+++ For Loop/src/song/SongThatDoesntEnd.java
@@ -4,6 +4,6 @@ package song;
 public class SongThatDoesntEnd {
 
 	public static void main(String[] args) {
-		for(int i = 1; i <= 100; i++) System.out.println("This is the song that doesn't end\nYes, it goes on and on my friend.\nSome people started singing it " + i + " times ago\nNot knowing what it was.\nAnd they'll continue singing it " + (100 - i) + " more times just because");
+		for(int i = 1; i <= 100; i++) System.out.println("This is the song that doesn't end\nYes, it goes on and on my friend.\nSome people started singing it " + i + " times ago\nNot knowing what it was." + (i == 100 ?  "" : "\nAnd they'll continue singing it " + (100 - i) + " more times just because"));
 	}
 }
