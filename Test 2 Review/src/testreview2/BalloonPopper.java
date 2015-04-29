e81be2cb4bd304d5d75031af770a6e59cf309bcf
Paul Friederichsen <floogulinc@gmail.com>
2015-04-29 09:13:28 -0500

    Changed the test 2 review

--- Test 2 Review/src/testreview2/BalloonPopper.java
+++ Test 2 Review/src/testreview2/BalloonPopper.java
@@ -9,17 +9,16 @@ public class BalloonPopper {
 		int a = b.getAir();
 		double t = b.getThickness();
 		
-		for(int i = 0; i < 100; i++) {
-			b.setAir(i);
+		for(a = 0; a < 100; a++) {
 			if(a == 7) {
 				System.out.println("the balloon popped");
 				break;
 			}
 		}
 		
-		b.setThickness(20);
+		t = 20;
 		while(t > 10) {
-			b.setThickness(t - 1.5);
+			t =- 1.5;
 			if(t < 12) {
 				System.out.println("the balloon popped");
 				break;
